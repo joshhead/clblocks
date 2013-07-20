07ffbeed69b61c20679007cb55cc0ed85d67dd51
Josh Headapohl <joshhead@gmail.com>
2013-07-20 14:46:04 -0400

    Fix errors in previous commit

--- src/cltetris/core.clj
+++ src/cltetris/core.clj
@@ -4,7 +4,8 @@
             (java.awt.event KeyListener KeyEvent WindowAdapter)
             (java.awt.image BufferedImage)))
 
-(def keysdown (ref #{}))
+(defonce keysdown (ref #{}))
+(defonce running (ref true))
 
 (defn remove-key-listeners
   [component]
@@ -32,6 +33,7 @@
         close-listener (proxy [WindowAdapter] []
                          (windowClosing
                            [event]
+                           (dosync (alter running (constantly false)))
                            (System/exit 0)))]
     (doto frame
       (.addKeyListener key-listener)
@@ -41,6 +43,10 @@
   "Accepts a frame and a drawing function.
   Draws on a backing graphic and then copies
   the backing graphic to the frame.
+
+  Backing graphic will have clipRect set to the
+  size of the frame.
+
   (buffered-draw frame (fn [g] (draw-stuff g)))"
   [frame f]
   (let [width    (.getWidth frame)
@@ -48,50 +54,49 @@
         back-img (BufferedImage. width height BufferedImage/TYPE_4BYTE_ABGR)
         back-g   (.createGraphics back-img)
         g        (.getGraphics frame)]
+    (.setClip back-g 0 0 width height)
     (f back-g)
     (.drawImage g back-img 0 0 nil))
   frame)
 
 (defn draw-square
-  [frame direction]
-  (buffered-draw frame
-            (fn [g]
-              (let [width    (.getWidth frame)
-                    height   (.getHeight frame)
-                    [x y] (case direction
-                            :up [100 0]
-                            :down [100 200]
-                            :left [0 100]
-                            :right [200 100]
-                            :center [100 100]
-                            [100 100])]
-                (doto g
-                  (.setColor (Color. 200 200 200))
-                  (.fillRect 0 0 width height)
-                  (.setColor (Color. 0 100 0))
-                  (.fillRect x y 100 100))))))
+  [g direction]
+  (let [width  (.-width  (.getClipBounds g))
+        height (.-height (.getClipBounds g))
+        [x y]  (case direction
+                 :up [100 0]
+                 :down [100 200]
+                 :left [0 100]
+                 :right [200 100]
+                 :center [100 100]
+                 [100 100])]
+    (doto g
+      (.setColor (Color. 200 200 200))
+      (.fillRect 0 0 width height)
+      (.setColor (Color. 0 100 0))
+      (.fillRect x y 100 100))))
 
 (defn -main
   "Start the show"
   [& args]
   (defonce frame (Frame.))
-  (defonce running (ref true))
   (.show frame)
-  (.setSize frame 500 500)
+  (.setSize frame 300 300)
   (setup-frame frame)
   (dosync (alter running (constantly true)))
-  (.start (Thread. (fn [] (while @running (do
-                                            (draw-square frame (first @keysdown))
-                                            (Thread/sleep 33))))))
-  #_(.start (Thread. (fn [] (while @running (do
-                                            (println (first @keysdown))
-                                            (Thread/sleep 33)))))))
+  (let [bdf (partial buffered-draw frame)]
+    (.start (Thread. (fn [] (while @running (do
+                                              (bdf #(draw-square % (first @keysdown)))
+                                              (Thread/sleep 33))))))))
 
 (comment
   "Commands to manually contol the thread flag"
   (dosync (alter running (constantly true)))
   (dosync (alter running (constantly false)))
 
+  (.start (Thread. (fn [] (while @running (do
+                                            (println (first @keysdown))
+                                            (Thread/sleep 33))))))
   "Close frame"
   (doto frame
     remove-key-listeners
