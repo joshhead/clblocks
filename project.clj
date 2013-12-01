a1bb729b8ed5d872466e79a0286b65cd7a184cff
Josh Headapohl <joshhead@gmail.com>
2013-12-01 13:36:14 -0500

    Add cljx plugin dependency and some configuration

--- project.clj
+++ project.clj
@@ -4,5 +4,27 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]])
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+                 [org.clojure/clojurescript "0.0-2080"]]
+
+  :plugins [[com.keminglabs/cljx "0.3.1"]
+            [lein-cljsbuild "1.0.1-SNAPSHOT"]]
+
+  :source-paths ["src/clj" "target/generated/clj"]
+
+  :hooks [cljx.hooks]
+
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/generated/clj"
+                   :rules :clj}
+
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/generated/cljs"
+                   :rules :cljs}]}
+
+  :cljsbuild {:builds [{:source-paths ["src/cljs" "target/generated/cljs"]
+                        :compiler {:output-to "web/cltetris.js"
+                                   :optimizations :advanced
+                                   :pretty-print true}}]})
