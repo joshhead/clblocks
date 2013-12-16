5c744ac968bd96e524fcb973da68de1e32171e20
Josh Headapohl <joshhead@gmail.com>
2013-12-15 23:58:25 -0500

    Add alternate cljs rendering code using om

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [org.clojure/clojurescript "0.0-2120"]]
+                 [org.clojure/clojurescript "0.0-2122"]
+                 [om "0.1.0"]]
 
   :plugins [[com.keminglabs/cljx "0.3.1"]
             [lein-cljsbuild "1.0.1-SNAPSHOT"]]
@@ -29,7 +30,10 @@
                         :compiler {:output-to "web/cltetris.js"
                                    :output-dir "web/out"
                                    :optimizations :none
-                                   :source-map true}}
+                                   :source-map true
+                                   :foreign-libs [{:file "om/react.js"
+                                                   :provides ["React"]}]
+                                   :externs ["om/externs/react.js"]}}
                        {:id "prod"
                         :source-paths ["src/cljs" "target/generated/cljs"]
                         :compiler {:output-to "web/cltetris.js"
--- src/cljs/cltetris/platform.cljs
+++ src/cljs/cltetris/platform.cljs
@@ -2,9 +2,11 @@
   (:require [cltetris.game :as cltetris]
             [clojure.string :as clojure.string]
             [cljs.core.async :as async]
-            [goog.dom :as dom]
+            [goog.dom :as gdom]
             [goog.events :as events]
-            [goog.events.KeyCodes])
+            [goog.events.KeyCodes]
+            [om.core :as om]
+            [om.dom :as dom :include-macros true])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (def keycodes {goog.events.KeyCodes.UP :up
@@ -14,11 +16,16 @@
                goog.events.KeyCodes.SPACE :drop
                goog.events.KeyCodes.ESC :escape})
 
+(def game-states (async/chan))
+
+(declare setup-game-om-root)
+
 (defn new-frame
   ([]
      (new-frame 200 440))
   ([width height]
-     (dom/getElement "cltetris")))
+     (setup-game-om-root "om" game-states)
+     (gdom/getElement "cltetris")))
 
 (defn get-event-keyword
   [e]
@@ -70,16 +77,81 @@
   [game]
   (str "<div class='cltetris__status'>" (next-markup game) (score-markup game) "</div>"))
 
-(defn ^:private game-markup
+(defn game-merged-grid
+  "Take a game and return the grid with the current piece merged in"
   [{:keys [grid piece position] :as game}]
-  (let [drawable-grid (cltetris/merge-grid grid piece position)]
-    (str "<div class='cltetris__field'>" (grid-markup drawable-grid) "</div>"
-         (status-markup game))))
+  (cltetris/merge-grid grid piece position))
+
+(defn ^:private game-markup
+  [game]
+  (str "<div class='cltetris__field'>" (grid-markup (game-merged-grid game)) "</div>"
+       (status-markup game)))
 
 (defn frame-draw-game
   [frame game]
+  (async/put! game-states game)
   (set! (.-innerHTML frame) (game-markup game)))
 
 (defn setup-close-listener
   [frame]
   (async/chan))
+
+(defn om-cell
+  [cell]
+  (if (> cell 0)
+    (dom/div #js {:className "cltetris__cell--full"})
+    (dom/div #js {:className "cltetris__cell--empty"})))
+
+(defn om-row
+  [row]
+  (dom/div
+   #js {:className "row"}
+   (into-array
+    (concat
+     (map om-cell row)))))
+
+(defn om-grid
+  [grid]
+  (dom/div
+   #js {:className "cltetris__grid"}
+   (into-array (concat (map om-row grid)))))
+
+(defn om-next
+  [{:keys [next]}]
+  (dom/div
+   #js {:className "cltetris__status__next"}
+   (array "Next: " (om-grid next))))
+
+(defn om-score
+  [{:keys [lines]}]
+  (dom/div
+   #js {:className "cltetris__status__score"}
+   (str "Lines: " lines)))
+
+(defn setup-game-om-root
+  "id: the id of an element to mount the component at
+  game-states: a channel. Put cltetris game states on it"
+  [id game-states]
+    (om/root
+     {:game nil}
+     (fn [data]
+       (reify
+         dom/IWillMount
+         (-will-mount [_ _]
+           (go (while true
+                 (om/replace! data [:game] (async/<! game-states)))))
+         dom/IRender
+         (-render [_ _]
+           (dom/div
+            nil
+            (when-let [game (:game data)]
+              (array
+               (dom/div
+                #js {:className "cltetris__field"}
+                (om-grid (game-merged-grid (:game data))))
+               (dom/div
+                #js {:className "cltetris__status"}
+                (array
+                 (om-next (:game data))
+                 (om-score (:game data))))))))))
+     (gdom/getElement id)))
