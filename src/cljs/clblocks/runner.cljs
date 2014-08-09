b8092aeb1f611dcd6b37069d16fcefb187d699a3
Josh Headapohl <joshhead@gmail.com>
2014-08-09 17:48:46 -0400

    Pause ticker when game is paused

--- src/cljs/clblocks/runner.cljs
+++ src/cljs/clblocks/runner.cljs
@@ -6,26 +6,58 @@
             [om.core :as om :include-macros true])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
+(def show-timing? (atom false))
+
+(defn ^:export show-timing
+  [show?]
+  (if show?
+    (reset! show-timing? true)
+    (reset! show-timing? false)))
+
 (defn- now
   []
   (.valueOf (new js/Date)))
 
 (defn tick-chan
+  "Return a vector of two channels [ticker control].
+  Reads on ticker will return tick every n milliseconds
+  where n is the argument supplied to this function.
+  Writing :pause to the control channel will cause ticker
+  to park until value :unpause is placed on control.
+
+  It is safe to put :pause or :unpause twice in a row,
+  values other than the expected :pause or :unpause will
+  be ignored."
   [ms]
   (let [ticker (async/chan)
         control (async/chan)]
     (go-loop [start (now)
-              timeout ms]
-      (let [[val port] (async/alts! [control (async/timeout timeout)])]
-        (if (= val :pause)
-          (let [elapsed (- (now) start)
-                diff (- ms elapsed)
-                remaining (if (pos? diff) diff ms)]
-            (async/<! control)
-            (recur (now) remaining))
-          (do
-            (async/>! ticker :tick)
-            (recur (now) ms)))))
+              timeout (async/timeout ms)]
+      (let [[val port] (async/alts! [control timeout])]
+        (cond
+         ; non :pause value on control channel, ignore
+         (and (= port control) (not= val :pause))
+         (recur start timeout)
+
+         ; got :pause on control channel, wait for :unpause
+         (and (= port control) (= val :pause))
+         (let [elapsed (- (now) start)
+                  diff (- ms elapsed)
+                  remaining (if (pos? diff) diff ms)]
+           (if @show-timing?
+             (.log js/console (- (now) start)))
+              (loop []
+                (when (not= (async/<! control) :unpause)
+                  (recur)))
+              (recur (now) (async/timeout remaining)))
+
+         ; timeout, put :tick and start over
+         :else
+         (do
+           (async/>! ticker :tick)
+           (if @show-timing?
+             (.log js/console (- (now) start)))
+           (recur (now) (async/timeout ms))))))
     [ticker control]))
 
 (defn play
@@ -41,12 +73,18 @@
               key (if (= port keysc) val :down)
               next-game (clblocks/step-game game key)]
 
+          ; Trigger om rerender
           (swap! app-state #(assoc % :game next-game))
+
+          ; Pause/unpause ticker to match game
+          (if (clblocks/paused? next-game)
+            (async/put! tick-control :pause)
+            (async/put! tick-control :unpause))
 
           (when-not (or (nil? key) (clblocks/game-over? next-game))
             (recur next-game))))
 
-      ; Unpause game after it ends, game-over+paused is redundant
+      ; Unpause game after it ends. Pause after game-over is unnecessary
       (swap! app-state #(update-in % [:game] clblocks/unpause)))
 
     app-state))
